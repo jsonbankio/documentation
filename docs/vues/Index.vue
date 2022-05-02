2455443f14c117d2f197a8ed7f50b43fc94b6c0d
trapcodeio <trapcode.io@gmail.com>
2022-05-02 18:04:48 +0100

    Test vue

--- docs/vues/Index.vue
+++ docs/vues/Index.vue
@@ -1,8 +1,14 @@
-
 <template>
   <h3>JsonBank Documentation</h3>
+  <p>This is from the <b>index.vue</b> file</p>
+
+  <p>{{counter}}</p>
+
+  <button @click="counter++">Click me</button>
 </template>
 
 <script setup>
-console.log('setup');
+import {ref} from 'vue'
+
+const counter = ref(0);
 </script>
